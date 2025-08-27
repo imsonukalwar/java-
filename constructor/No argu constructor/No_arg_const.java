ae3dfceb27bcd7ea6ce7ec01c2ee3cc0ca42d023
unknown <imsonukalwar@gmail.com>
2025-08-27 09:28:23 +0530

    no arguement constructor

---
+++ constructor/No argu constructor/No_arg_const.java
@@ -0,0 +1,10 @@
+public class No_arg_const{
+    static class t1{
+        public t1(){
+            System.out .println("No-argument constructor called!");
+        }
+    }
+    public static void main(String args[]){
+        t1 cons=new t1();
+    }
+}
