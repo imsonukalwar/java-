c83e4bd2305021ef703bed198ff255154c85cccc
unknown <imsonukalwar@gmail.com>
2025-08-27 09:18:00 +0530

    constructor type

---
+++ constructor/defaout constructor/defaout.java
@@ -0,0 +1,9 @@
+public class defaout{
+    int age;
+    public static void main(String[] args) {
+        defaout d=new defaout();//defaoult constructor are created itself while we are ceating a object 
+        //compile automatic creat a constuctor
+        d.age=45;
+        System.out .println(d.age);
+    }
+}
