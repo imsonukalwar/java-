c50e1adc52b7e501ec07b02088e64e531c6d42bf
unknown <imsonukalwar@gmail.com>
2025-08-27 10:06:49 +0530

    paramiterize const

---
+++ constructor/paramitrize constructor/cons.java
@@ -0,0 +1,12 @@
+public class cons{
+    static class cl{
+        public cl(String name){
+            System.out.println(name);
+        }
+    }
+    public static void main(String[] args) {
+        cl c=new cl("sonu");
+        cl c1=new cl("monu");
+
+    }
+}
