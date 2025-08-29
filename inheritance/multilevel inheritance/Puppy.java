ca9a133840254613da5bc32054b044f93c44cc6b
unknown <imsonukalwar@gmail.com>
2025-08-29 14:01:33 +0530

    multilevel inheritance

---
+++ inheritance/multilevel inheritance/Puppy.java
@@ -0,0 +1,27 @@
+
+
+
+    class Animal {
+    void eat() {
+        System.out.println("Animal is eating...");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog is barking...");
+    }
+}
+
+public class Puppy extends Dog {
+    void weep() {
+        System.out.println("Puppy is weeping...");
+    }
+    public static void main(String[] args) {
+        Puppy p = new Puppy();
+        p.eat();   // From Animal
+        p.bark();  // From Dog
+        p.weep();  // From Puppy
+    }
+}
+
