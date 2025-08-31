fe9afc2455727d30085a0f823a7eaddb36adb6cd
unknown <imsonukalwar@gmail.com>
2025-08-31 09:49:46 +0530

    update

--- constructor/paramitrize constructor/cons.java
+++ constructor/paramitrize constructor/cons.java
@@ -1,12 +1,21 @@
-public class cons{
-    static class cl{
+
+    class cl{
         public cl(String name){
             System.out.println(name);
         }
+        public cl(int n){//here we are doing constructor overloading with diff arguement
+            System.out.println(n);
+        }
+        public cl(char t){
+            System.out.println(t);
+        }
     }
+    public class cons{
     public static void main(String[] args) {
         cl c=new cl("sonu");
-        cl c1=new cl("monu");
+        cl c2=new cl('t');
+        cl c3=new cl(3);
+
 
     }
 }
